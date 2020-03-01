89e68dd21c1e7c93fab746c3d316c72d26d089f8
Megan Murphy <childovhurin@gmail.com>
2020-03-01 18:47:41 -0500

    updated html framework, css elements, and first click event to launch quiz

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+//Variables and arrays
+var questions = ["question1", "question2", "question3", "question4", "question5"];
+
+var question1 = ["option1", "option2", "option3", "option4"];
+var question2 = ["option1", "option2", "option3", "option4"];
+var question3 = ["option1", "option2", "option3", "option4"];
+var question4 = ["option1", "option2", "option3", "option4"];
+var question5 = ["option1", "option2", "option3", "option4"];
+
+//First Click Event to launch game
+var button = document.getElementById("start");
+button.addEventListener("click", start);
+   console.log("clicked");
+
+//event listeners for correct or incorrect answers for loops
+
+//Finish screen with score calculation
+
+//Enter initials for high score with submit option, 'go back' and 'clear high score' buttons
+
+
