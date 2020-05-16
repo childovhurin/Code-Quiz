63bb11bdf4e69647faf85c8f8e417ff13b763952
Megan Murphy <childovhurin@gmail.com>
2020-05-16 17:13:18 -0400

    reworking js fucntions timer and onclick event revised

--- script.js
+++ script.js
@@ -1,90 +1,37 @@
-//QUESTIONS
-var questions = [
-    {
-        title: "Inside which HTML element do we put the JavaScript?",
-        choices: ["<SCRIPT>", "<BODY>", "<HEAD>", " <TITLE>"],
-        answer: "<SCRIPT>"
-    },
-    {
-        title: "______ tag is an extension to HTML that can enclose any number of JavaScript statements.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },
-    {
-        title: "Inside which HTML element do we put the JavaScript?",
-        choices: ["<SCRIPT>", "<BODY>", "<HEAD>", " <TITLE>"],
-        answer: "<SCRIPT>"
-    },
-    {
-        title: "JavaScript entities start with _______ and end with _________.",
-        choices: ["Semicolon, colon", "Semicolon, Ampersand", "Ampersand, colon", "Ampersand, semicolon"],
-        answer: "Ampersand, semicolon"
-    },
-    {
-        title: "What is mean by the this keyword in Javascript?",
-        choices: ["It refers current object", "It referes previous object", "It is variable which contains value", "None of the above"],
-        answer: "It refers current object"
-    },
+//Variables
+var timerEl = document.querySelector("#timer");
+var timerDiv = document.querySelector(".timer-div");
+var secondsLeft = 100;
+var quizBeginEl = document.querySelector("#quiz-begin");
+var quizEl = document.querySelector("#quiz");
+var scoreSpan = document.querySelector("#score-span");
+var highScoreEl = document.querySelector(".high-score");
 
-];
+quizBeginEl.innerHTML = "<h4>Click Here to Begin</h4>";
 
-//VARIABLES
+//Timer function
+var timer;
 
-var startBtn = document.getElementById("startBtn");
-var submitBtn = document.querySelector("button.submitBtn")
-//var secondsLeft = (questions.length * 12 + 1);
-var timerEl = document.getElementById("timer");
-var submitScoreEl = document.querySelector("#submit-score");
-var userScoreEl = document.getElementById("user-score");
-var questionHead = document.getElementById("questions");
-// var answerChoices = document.getElementById("answers");
+function countDown() {
 
-var answer;
-var userNameInput;
-var button = document.getElementById("start");
-
-
-
-//First Click Event to launch game
-
-startBtn.addEventListener("click", start);
-
-function start() {
-    console.log(questions)
-    var question = questions.shift()
-    var questionsDiv = document.createElement("div")
-    questionsDiv.textContent = question.title;
-    console.log(questionHead)
-    questionHead.prepend(questionsDiv)
-    var questionChoices = question.choices;
-    var answerChoices = document.getElementById("answers");
-    console.log(answerChoices)
-    for (i = 0; i < questionChoices.length; i++) {
-        var choiceButton = document.createElement("button")
-        choiceButton.textContent = questionChoices[i]
-        answerChoices.appendChild(choiceButton);
+    if (secondsLeft < 1) {
+        timerDiv.textContent = "Time's Up!";
+        clearInterval(timer);
+        endOfQuiz();
+    } else {
+        timerEl.textContent = secondsLeft;
+        secondsLeft--;
     }
+}
 
-    var count = 15;
-    var interval = setInterval(function () {
-        document.getElementById('timer').innerHTML = count;
-        count--;
-        if (count === 0) {
-            clearInterval(interval);
-            document.getElementById('timer').innerHTML = 'Done';
-        }
-    }, 1000);
-};
+//Click event to start quiz
+quizBeginEl.addEventListener("click", function () {
+    quizBeginEl.textContent = "";
+    timer = setInterval(countDown, 1000);
+    countDown();
+    questionCreator(firstQuestion, makeFirstChoice);
+  
+})
 
-
-//TIMER - click button start button, timer starts and first question pops up
-
-
-
-
-
-//Finish screen with score calculation
-
-//Enter initials for high score with submit option, 'go back' and 'clear high score' buttons
 
 
